92f228eade21e375c1ec6a5954c1bbcdc1fd1be3
green <14atcamposo@gmail.com>
2023-11-09 16:48:34 -0500

    CAMBIOS 2

--- src/app/service/university.service.ts
+++ src/app/service/university.service.ts
@@ -35,5 +35,39 @@ export class UniversityService {
     });
     return this.http.get<any>(this.url + "/university/" + id, {headers});
   }
-
+  //filtro para encontrar universidad por nombre
+  listUniversitiesByName(name:string){
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    });
+    return this.http.get<University[]>(`${this.url}/universities/findby/${name}`, { headers });
+  }
+  //filtro de busqueda avanzada
+  listUniversitiesAdvancedFilter(district:string, modality:string, qFrom:number, qTo:number, pFrom:number, pTo:number){
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    });
+    return this.http.get<University[]>(`${this.url}/universities/findbyfilters/${district}/${modality}/${qFrom}/${qTo}/${pFrom}/${pTo}`, { headers });
+  }
+  //filtro de tipo de educacion
+  listUniversitiesByEducationType(educationType:string){
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    });
+    return this.http.get<University[]>(`${this.url}/universities/findbyeducationtype/${educationType}`, { headers });
+  }
+  //filtro de pension promedio
+  listUniversitiesByPensionProm(pFrom:number, pTo:number){
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    });
+    return this.http.get<University[]>(`${this.url}/universities/findbypension/${pFrom}/${pTo}`, { headers });
+  }
+  //implementar filtro de link de matricula
+  getEnrrollmentLinkByUniversity(id:number){
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    });
+    return this.http.get<University[]>(`${this.url}/universities/findlinkbyid/${id}`, { headers });
+  }
 }
